be63493137b2dd57f7b2e53eb10b5856b953d7fc
Marcelo Morais <marcelo.fernandes@squidit.com.br>
2024-10-03 10:51:57 -0300

    atualização de lint e packages

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,8 @@
 import { GetObjectCommandOutput } from '@aws-sdk/client-s3';
-import { z } from 'czod';
-export { z as cz } from 'zod';
 
 interface Logger {
+    Error: (dataToLog: Error | Record<string, unknown>, request?: unknown, response?: unknown, user?: string) => void;
     Info: (message: Record<string, unknown>) => void;
-    Error: (dataToLog: Record<string, unknown> | Error, req?: unknown, res?: unknown, user?: string) => void;
 }
 interface ErrorConverter {
     Create: (settings: Record<string, unknown>, originalError: unknown) => Error;
@@ -22,15 +20,15 @@ declare class CreatorsCloudStorageClient {
         compress?: boolean;
     }): Promise<void>;
     downloadFile(bucketName: string, fileName: string): Promise<GetObjectCommandOutput>;
-    downloadJson<T extends z.ZodType>(bucketName: string, fileName: string, schema: T): Promise<z.infer<T>>;
     createSignedUploadUrl(bucketName: string, directory: string, fileName: string, contentType: ContentType, expirationInSeconds: number): Promise<{
-        signedUrl: string;
         publicUrl: string;
+        signedUrl: string;
     }>;
     createSignedDownloadUrl(bucketName: string, directory: string, fileName: string, expirationInSeconds: number): Promise<string>;
     private translateContentTypeToExtension;
+    private stripQueryString;
 }
 declare const possibleContentTypes: readonly ["image/jpg", "image/jpeg", "image/png", "application/pdf"];
 type ContentType = typeof possibleContentTypes[number];
 
-export { type ContentType, CreatorsCloudStorageClient };
+export { type ContentType, CreatorsCloudStorageClient, possibleContentTypes };
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import { GetObjectCommand, PutObjectCommand, S3Client, type GetObjectCommandInput, type GetObjectCommandOutput, type PutObjectCommandInput } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { type NodeJsClient } from '@smithy/types'
-import { type z as cz } from 'czod'
-import { promisify } from 'util'
-import { gzip as callbackGzip, createGunzip } from 'zlib'
+import { promisify } from 'node:util'
+import { gzip as callbackGzip } from 'node:zlib'
 const gzip = promisify(callbackGzip)
 
 interface Logger {
+  Error: (dataToLog: Error | Record<string, unknown>, request?: unknown, response?: unknown, user?: string,) => void
   Info: (message: Record<string, unknown>) => void
-  Error: (dataToLog: Record<string, unknown> | Error, req?: unknown, res?: unknown, user?: string,) => void
 }
 
 interface ErrorConverter {
@@ -16,22 +15,22 @@ interface ErrorConverter {
 }
 
 export class CreatorsCloudStorageClient {
-  private static instance: CreatorsCloudStorageClient
+  private static instance: CreatorsCloudStorageClient | undefined
   private readonly s3Client: NodeJsClient<S3Client>
 
-  private constructor (private readonly region: string, accessKeyId: string, secretAccessKey: string, private readonly loggerInstance: Logger, private readonly errorConverter: ErrorConverter) {
+  private constructor(private readonly region: string, accessKeyId: string, secretAccessKey: string, private readonly loggerInstance: Logger, private readonly errorConverter: ErrorConverter) {
     this.loggerInstance = loggerInstance
     this.errorConverter = errorConverter
     this.s3Client = new S3Client({
-      region,
       credentials: {
         accessKeyId,
-        secretAccessKey
-      }
+        secretAccessKey,
+      },
+      region,
     })
   }
 
-  public static getInstance (): CreatorsCloudStorageClient {
+  public static getInstance(): CreatorsCloudStorageClient {
     if (!CreatorsCloudStorageClient.instance) {
       throw new Error(`${CreatorsCloudStorageClient.name} not initialized`)
     }
@@ -39,124 +38,91 @@ export class CreatorsCloudStorageClient {
     return CreatorsCloudStorageClient.instance
   }
 
-  public static init (region: string, accessKeyId: string, secretAccessKey: string, loggerInstance: Logger, errorConverter: ErrorConverter): void {
+  public static init(region: string, accessKeyId: string, secretAccessKey: string, loggerInstance: Logger, errorConverter: ErrorConverter): void {
     this.instance = new CreatorsCloudStorageClient(region, accessKeyId, secretAccessKey, loggerInstance, errorConverter)
   }
 
-  public async uploadFile (bucketName: string, fileName: string, fileContent: Buffer, options: { compress?: boolean } = {}): Promise<void> {
+  public async uploadFile(bucketName: string, fileName: string, fileContent: Buffer, options: { compress?: boolean } = {}): Promise<void> {
     try {
-      const params: PutObjectCommandInput = {
+      const parameters: PutObjectCommandInput = {
+        Body: options.compress ? await gzip(fileContent) : fileContent,
         Bucket: bucketName,
         Key: fileName,
-        Body: options.compress ? await gzip(fileContent) : fileContent,
-        ...options.compress && { ContentEncoding: 'gzip' }
+        ...options.compress && { ContentEncoding: 'gzip' },
       }
-      const command = new PutObjectCommand(params)
+      const command = new PutObjectCommand(parameters)
 
       if (options.compress) {
-        console.debug(`Compression percentage: ${(((params.Body as Buffer)?.length / fileContent.length) * 100).toFixed(2)}% of original size`)
+        console.debug(`Compression percentage: ${(((parameters.Body as Buffer).length / fileContent.length) * 100).toFixed(2)}% of original size`)
       }
 
       const response = await this.s3Client.send(command)
 
       console.debug(`Upload of file ${fileName} to bucket ${bucketName} successful. Response: ${JSON.stringify(response)}`)
-    } catch (error) {
-      this.loggerInstance.Error(this.errorConverter.Create({ message: 'Cloud storage file upload failure', code: 'CCSC001', detail: { bucketName, fileName, fileContent } }, error))
+    }
+    catch (error) {
+      this.loggerInstance.Error(this.errorConverter.Create({ code: 'CCSC001', detail: { bucketName, fileContent, fileName }, message: 'Cloud storage file upload failure' }, error))
       throw error
     }
   }
 
-  public async downloadFile (bucketName: string, fileName: string): Promise<GetObjectCommandOutput> {
+  public async downloadFile(bucketName: string, fileName: string): Promise<GetObjectCommandOutput> {
     try {
-      const params: GetObjectCommandInput = {
+      const parameters: GetObjectCommandInput = {
         Bucket: bucketName,
-        Key: fileName
+        Key: fileName,
       }
-      const command = new GetObjectCommand(params)
+      const command = new GetObjectCommand(parameters)
 
       const response = await this.s3Client.send(command)
 
       console.debug(`Download of file ${fileName} from bucket ${bucketName} successful.`)
       return response
-    } catch (error) {
-      this.loggerInstance.Error(this.errorConverter.Create({ message: 'Cloud storage file download failure', code: 'CCSC002', detail: { bucketName, fileName } }, error))
+    }
+    catch (error) {
+      this.loggerInstance.Error(this.errorConverter.Create({ code: 'CCSC002', detail: { bucketName, fileName }, message: 'Cloud storage file download failure' }, error))
       throw error
     }
   }
 
-  public async downloadJson<T extends cz.ZodType> (bucketName: string, fileName: string, schema: T): Promise<cz.infer<T>> {
-    const downloadResult = await this.downloadFile(bucketName, fileName)
-    if (!downloadResult.Body) {
-      throw new Error('No body in file content')
-    }
-
-    let jsonString: string
-    if (downloadResult.ContentEncoding === 'gzip') {
-      const fileBuffer = Buffer.from(await downloadResult.Body.transformToByteArray())
-      console.log(`Decompressing file ${fileName} from bucket ${bucketName}`)
-
-      const unzippedResult: Buffer = await new Promise((resolve, reject) => {
-        const gunzip = createGunzip()
-        gunzip.on('data', decompressedChunk => {
-          resolve(Buffer.concat([decompressedChunk]))
-        })
-
-        gunzip.on('error', (err) => {
-          reject(err)
-        })
-
-        gunzip.write(fileBuffer)
-        gunzip.end()
-      })
-
-      jsonString = unzippedResult.toString()
-    } else {
-      jsonString = await downloadResult.Body.transformToString()
-    }
-    const json = JSON.parse(jsonString)
-    return schema.parse(json)
-  }
-
-  public async createSignedUploadUrl (bucketName: string, directory: string, fileName: string, contentType: ContentType, expirationInSeconds: number): Promise<{ signedUrl: string, publicUrl: string }> {
+  public async createSignedUploadUrl(bucketName: string, directory: string, fileName: string, contentType: ContentType, expirationInSeconds: number): Promise<{ publicUrl: string, signedUrl: string }> {
     const extension = this.translateContentTypeToExtension(contentType)
     const command = new PutObjectCommand({
       Bucket: bucketName,
-      Key: `${directory}/${fileName}.${extension}`
+      Key: `${directory}/${fileName}.${extension}`,
     })
 
     const signedUrl = await getSignedUrl(this.s3Client, command, { expiresIn: expirationInSeconds })
     return {
+      publicUrl: this.stripQueryString(signedUrl),
       signedUrl,
-      publicUrl: this.stripQueryString(signedUrl)
     }
   }
 
-  public async createSignedDownloadUrl (bucketName: string, directory: string, fileName: string, expirationInSeconds: number): Promise<string> {
+  public async createSignedDownloadUrl(bucketName: string, directory: string, fileName: string, expirationInSeconds: number): Promise<string> {
     const command = new GetObjectCommand({
       Bucket: bucketName,
-      Key: `${directory}/${fileName}`
+      Key: `${directory}/${fileName}`,
     })
 
     return await getSignedUrl(this.s3Client, command, { expiresIn: expirationInSeconds })
   }
 
-  private translateContentTypeToExtension (contentType: ContentType): string {
+  private translateContentTypeToExtension(contentType: ContentType): string {
     return {
+      'application/pdf': 'pdf',
       'image/jpeg': 'jpg',
       'image/jpg': 'jpg',
       'image/png': 'png',
-      'application/pdf': 'pdf'
     }[contentType]
   }
 
-  private stripQueryString (urlString: string): string {
+  private stripQueryString(urlString: string): string {
     const url = new URL(urlString)
     url.search = ''
     return url.toString()
   }
 }
 
-const possibleContentTypes = ['image/jpg', 'image/jpeg', 'image/png', 'application/pdf'] as const
+export const possibleContentTypes = ['image/jpg', 'image/jpeg', 'image/png', 'application/pdf'] as const
 export type ContentType = typeof possibleContentTypes[number]
-
-export { z as cz } from 'zod'
